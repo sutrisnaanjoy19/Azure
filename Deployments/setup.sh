f02109e2c76ec2401c9217415b70f18b1592e1ab
sutrisnaanjoy19 <anjoysutrisna19@gmail.com>
2024-06-17 09:29:00 +0530

    adding deployments files

--- Deployments/setup.sh
+++ Deployments/setup.sh
@@ -1,7 +1,21 @@
-kubectl apply -f /home/sushi/Desktop/work/Azure/Deployments/test-deployment/namespace/nginx-namespace.yaml --cluster Testing-aks-EastUS
-kubectl apply -f /home/sushi/Desktop/work/Azure/Deployments/test-deployment/namespace/test-namespace.yaml --cluster Testing-aks-EastUS
+kubectl apply -f /home/sushi/Desktop/work/Azure/Deployments//namespace/nginx-namespace.yaml --cluster Testing-aks-EastUS
+
+kubectl apply -f /home/sushi/Desktop/work/Azure/Deployments/namespace/test-namespace.yaml --cluster Testing-aks-EastUS
+
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
 helm install misc-nginx ingress-nginx/ingress-nginx --values /home/sushi/Desktop/work/Azure/Deployments/nginx/values.yaml --kube-context Testing-aks-EastUS -n nginx
+
 kubectl apply -f /home/sushi/Desktop/work/Azure/Deployments/test-deployment/namespace/test-namespace.yaml --cluster Testing-aks-EastUS
+
 kustomize build /home/sushi/Desktop/work/Azure/Deployments/test-deployment
+
 kustomize build /home/sushi/Desktop/work/Azure/Deployments/test-deployment | kubectl apply -f - --cluster Testing-aks-EastUS -n test
+
+kubectl apply -f /home/sushi/Desktop/work/Azure/Deployments/namespace/monitoring-ns.yaml --cluster Testing-aks-EastUS
+
+helm install aks-prometheus prometheus-community/kube-prometheus-stack --values /home/sushi/Desktop/work/Azure/Deployments/Prometheus/values.yaml --kube-context Testing-aks-EastUS -n monitoring
+
+# your grafana default username: admin and password: prom-operator
+
+
